7f1f431736cd19b1ea5f8d8723b7044d2c63889f
NehuenZaloaga <nzaloaga@uade.edu.ar>
2026-04-13 00:38:17 -0300

    Add files via upload

---
+++ TP-paradigma/src/Modelo/Turno.java
@@ -0,0 +1,71 @@
+package Modelo;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Turno {
+    private Long id;
+    private Paciente paciente; //es una asociación
+    private Odontologo odontologo;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private EstadoTurno estado;
+
+    public Turno(Long id, Paciente paciente, Odontologo odontologo, LocalDate fecha, LocalTime hora) {
+        this.id = id;
+        this.paciente = paciente;
+        this.odontologo = odontologo;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Odontologo getOdontologo() {
+        return odontologo;
+    }
+
+    public void setOdontologo(Odontologo odontologo) {
+        this.odontologo = odontologo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    @Override
+    public String toString() {
+        return "Turno{" +
+                "id=" + id +
+                ", paciente=" + paciente +
+                ", odontologo=" + odontologo +
+                ", fecha=" + fecha +
+                ", hora=" + hora +
+                '}';
+    }
+}
